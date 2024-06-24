25f49f64b12d1d2b583008b924dd9f821ee68633
Leonardo Alves de Almeida <leonardoalves201510@gmail.com>
2024-06-24 11:59:04 -0300

    feat: add promo banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Header />
       <div className="px-5 pt-6">
         <Search />
@@ -13,6 +14,18 @@ export default function Home() {
       <div className="px-5 pt-6">
         <CategoryList />
       </div>
-    </div>
+
+      <div className="px-2 pt-6">
+        <Image
+          src="/banner-1.svg"
+          alt="Até 30% de desconto em pizzas"
+          width={0}
+          height={0}
+          className="h-auto w-full object-contain"
+          sizes="100vw"
+          quality={100}
+        />
+      </div>
+    </>
   );
 }
