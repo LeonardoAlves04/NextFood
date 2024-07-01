0651a7a328fe1c34e1815ac93f4ba9af191cc993
Leonardo Alves de Almeida <leonardoalves201510@gmail.com>
2024-07-01 11:07:53 -0300

    feat: add product list and product item component

--- app/_components/product-item.tsx
+++ app/_components/product-item.tsx
@@ -1,9 +1,18 @@
-import { Product } from "@prisma/client";
+import { Prisma } from "@prisma/client";
 import Image from "next/image";
 import { computeProductTotalPrice, formatCurrency } from "../_helpers/price";
+import { ArrowDownIcon } from "lucide-react";
 
 interface ProductItemProps {
-  product: Product;
+  product: Prisma.ProductGetPayload<{
+    include: {
+      restaurant: {
+        select: {
+          name: true;
+        };
+      };
+    };
+  }>;
 }
 
 const ProductItem = ({ product }: ProductItemProps) => {
@@ -16,6 +25,14 @@ const ProductItem = ({ product }: ProductItemProps) => {
           fill
           className="rounded-lg object-cover shadow-md"
         />
+        {product.discountPercentage && (
+          <div className="absolute left-2 top-2 flex items-center gap-[2px] rounded-full bg-primary px-2 py-[2px] text-white">
+            <ArrowDownIcon size={12} />
+            <span className="text-xs font-semibold">
+              {product.discountPercentage}%
+            </span>
+          </div>
+        )}
       </div>
 
       <div>
@@ -31,6 +48,9 @@ const ProductItem = ({ product }: ProductItemProps) => {
             </span>
           )}
         </div>
+        <span className="block text-xs text-muted-foreground">
+          {product.restaurant.name}
+        </span>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,14 @@ import Header from "./_components/header";
 import ProductList from "./_components/product-list";
 import Search from "./_components/search";
 import Image from "next/image";
+import { Button } from "./_components/ui/button";
+import { ChevronRightIcon } from "lucide-react";
 
 export default function Home() {
   return (
     <>
       <Header />
+
       <div className="px-5 pt-6">
         <Search />
       </div>
@@ -28,7 +31,17 @@ export default function Home() {
         />
       </div>
 
-      <div className="pt-6">
+      <div className="space-y-4 pt-6">
+        <div className="flex items-center justify-between px-5">
+          <h2 className="font-semibold">Pedidos Recomendados</h2>
+          <Button
+            variant="ghost"
+            className="h-fit p-0 text-primary hover:bg-transparent"
+          >
+            Ver todos
+            <ChevronRightIcon size={16} />
+          </Button>
+        </div>
         <ProductList />
       </div>
     </>
