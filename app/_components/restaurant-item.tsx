b4d8a66c7d5bb0c460d2d7b53c742e307e8fe1ab
Leonardo Alves de Almeida <leonardoalves201510@gmail.com>
2024-07-04 10:51:42 -0300

    chore: add favorite icon and star icon

--- app/_components/restaurant-item.tsx
+++ app/_components/restaurant-item.tsx
@@ -1,7 +1,8 @@
 import { Restaurant } from "@prisma/client";
-import { BikeIcon, TimerIcon } from "lucide-react";
+import { BikeIcon, HeartIcon, StarIcon, TimerIcon } from "lucide-react";
 import Image from "next/image";
 import { formatCurrency } from "../_helpers/price";
+import { Button } from "./ui/button";
 
 interface RestaurantItemProps {
   restaurant: Restaurant;
@@ -17,6 +18,18 @@ const RestaurantItem = ({ restaurant }: RestaurantItemProps) => {
           fill
           className="rounded-lg object-cover"
         />
+
+        <div className="absolute left-2 top-2 flex items-center gap-[2px] rounded-full bg-white px-2 py-[2px]">
+          <StarIcon size={12} className="fill-yellow-400 text-yellow-400" />
+          <span className="text-xs font-semibold">5.0</span>
+        </div>
+
+        <Button
+          size="icon"
+          className="absolute right-2 top-2 h-7 w-7 rounded-full bg-gray-700"
+        >
+          <HeartIcon size={16} className="fill-white" />
+        </Button>
       </div>
 
       <div>
