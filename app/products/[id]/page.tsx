c3b252010bb21365efd79603e062cf1c931286b6
Leonardo Alves de Almeida <leonardoalves201510@gmail.com>
2024-07-12 23:32:26 -0300

    chore: showing product name and product price

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/app/_components/ui/button";
+import { formatCurrency } from "@/app/_helpers/price";
 import { db } from "@/app/_lib/prisma";
 import { ChevronLeftIcon } from "lucide-react";
 import Image from "next/image";
@@ -43,7 +44,7 @@ const ProductPage = async ({ params: { id } }: ProductPageProps) => {
       </div>
 
       <div className="p-5">
-        <div className="flex items-center gap-1">
+        <div className="flex items-center gap-[0.375rem]">
           <div className="relative h-6 w-6">
             <Image
               src={product.restaurant.imageUrl}
@@ -55,6 +56,11 @@ const ProductPage = async ({ params: { id } }: ProductPageProps) => {
           <span className="text-xs text-muted-foreground">
             {product.restaurant.name}
           </span>
+        </div>
+        <h1 className="mb-2 mt-1 text-xl font-semibold">{product.name}</h1>
+
+        <div className="flex justify-between">
+          <h2>{formatCurrency(Number(product.price))}</h2>
         </div>
       </div>
     </div>
