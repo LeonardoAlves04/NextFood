53fb501820027cc255a6789f2b67ee4b9738c5cd
Leonardo Alves de Almeida <leonardoalves201510@gmail.com>
2024-07-12 23:25:03 -0300

    chore: showing restaurant name and image

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,11 +1,64 @@
+import { Button } from "@/app/_components/ui/button";
+import { db } from "@/app/_lib/prisma";
+import { ChevronLeftIcon } from "lucide-react";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
 interface ProductPageProps {
   params: {
     id: string;
   };
 }
 
-const ProductPage = ({ params: { id } }: ProductPageProps) => {
-  return <div>Product Page {id}</div>;
+const ProductPage = async ({ params: { id } }: ProductPageProps) => {
+  const product = await db.product.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      restaurant: true,
+    },
+  });
+
+  if (!product) {
+    return notFound();
+  }
+
+  return (
+    <div>
+      {/* IMAGEM */}
+      <div className="relative h-[360px] w-full">
+        <Image
+          src={product.imageUrl}
+          alt={product.name}
+          fill
+          className="object-cover"
+        />
+        <Button
+          className="absolute left-4 top-4 rounded-full text-foreground hover:text-white"
+          size="icon"
+        >
+          <ChevronLeftIcon />
+        </Button>
+      </div>
+
+      <div className="p-5">
+        <div className="flex items-center gap-1">
+          <div className="relative h-6 w-6">
+            <Image
+              src={product.restaurant.imageUrl}
+              alt={product.restaurant.name}
+              fill
+              className="rounded-full object-cover"
+            />
+          </div>
+          <span className="text-xs text-muted-foreground">
+            {product.restaurant.name}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductPage;
