76f3cdc9b1b452d39c583819160ce0c5b5e8d008
Leonardo Alves de Almeida <leonardoalves201510@gmail.com>
2024-07-15 22:01:50 -0300

    chore: componentizing product image for a better and semantic code

--- app/page.tsx
+++ app/page.tsx
@@ -58,7 +58,7 @@ const Home = async () => {
         <PromoBanner src="/banner-2.svg" alt="A partir de 17,90 em lanches" />
       </div>
 
-      <div className="space-y-4 pt-6">
+      <div className="pt-6">
         <div className="flex items-center justify-between px-5">
           <h2 className="font-semibold">Restaurantes Recomendados</h2>
           <Button
