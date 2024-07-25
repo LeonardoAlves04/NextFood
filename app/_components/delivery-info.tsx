e08211e41b350b97a5233ad136fe50c4a0ca1e06
Leonardo Alves de Almeida <leonardoalves201510@gmail.com>
2024-07-24 22:28:08 -0300

    feat: turn delivery info in a component

---
+++ app/_components/delivery-info.tsx
@@ -0,0 +1,50 @@
+import { BikeIcon, TimerIcon } from "lucide-react";
+import { formatCurrency } from "../_helpers/price";
+import { Card } from "./ui/card";
+import { Restaurant } from "@prisma/client";
+
+interface DeliveryInfoProps {
+  restaurant: Pick<Restaurant, "deliveryFee" | "deliveryTimeMinutes">;
+}
+
+const DeliveryInfo = ({ restaurant }: DeliveryInfoProps) => {
+  return (
+    <div>
+      <Card className="mt-6 flex justify-around py-3">
+        {/* CUSTO DA ENTREGA */}
+        <div className="flex flex-col items-center">
+          <div className="flex items-center gap-1 text-muted-foreground">
+            <span className="text-xs">Entrega</span>
+            <BikeIcon />
+          </div>
+
+          {Number(restaurant.deliveryFee) > 0 ? (
+            <p className="text-xs font-semibold">
+              {formatCurrency(Number(restaurant.deliveryFee))}
+            </p>
+          ) : (
+            <p className="text-xs font-semibold">Grátis</p>
+          )}
+        </div>
+
+        {/* TEMPO DA ENTREGA*/}
+        <div className="flex flex-col items-center">
+          <div className="flex items-center gap-1 text-muted-foreground">
+            <span className="text-xs">Tempo</span>
+            <TimerIcon />
+          </div>
+
+          {Number(restaurant.deliveryFee) > 0 ? (
+            <p className="text-xs font-semibold">
+              {Number(restaurant.deliveryTimeMinutes)} min.
+            </p>
+          ) : (
+            <p className="text-xs font-semibold">Grátis</p>
+          )}
+        </div>
+      </Card>
+    </div>
+  );
+};
+
+export default DeliveryInfo;
