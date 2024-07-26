505fce4962b5f8d034ffc26367ac08852d60820d
Leonardo Alves de Almeida <leonardoalves201510@gmail.com>
2024-07-26 17:27:57 -0300

    fix: fixing redirecting error in the link of product item

--- app/_components/restaurant-item.tsx
+++ app/_components/restaurant-item.tsx
@@ -11,54 +11,51 @@ interface RestaurantItemProps {
 
 const RestaurantItem = ({ restaurant }: RestaurantItemProps) => {
   return (
-    <Link
-      href={`restaurants/${restaurant.id}`}
-      className="min-w-[266px] max-w-[266px]"
-    >
-      <div className="space-y-3 py-6">
-        <div className="relative h-[136px] w-full">
+    <div className="min-w-[266px] max-w-[266px] space-y-3 py-6">
+      <div className="relative h-[136px] w-full">
+        <Link href={`restaurants/${restaurant.id}`}>
           <Image
             src={restaurant.imageUrl}
             alt={restaurant.name}
             fill
             className="rounded-lg object-cover"
           />
+        </Link>
 
-          <div className="absolute left-2 top-2 flex items-center gap-[2px] rounded-full bg-white px-2 py-[2px]">
-            <StarIcon size={12} className="fill-yellow-400 text-yellow-400" />
-            <span className="text-xs font-semibold">5.0</span>
-          </div>
-
-          <Button
-            size="icon"
-            className="absolute right-2 top-2 h-7 w-7 rounded-full bg-gray-700"
-          >
-            <HeartIcon size={16} className="fill-white" />
-          </Button>
+        <div className="absolute left-2 top-2 flex items-center gap-[2px] rounded-full bg-white px-2 py-[2px]">
+          <StarIcon size={12} className="fill-yellow-400 text-yellow-400" />
+          <span className="text-xs font-semibold">5.0</span>
         </div>
 
-        <div>
-          <h3 className="text-sm font-semibold">{restaurant.name}</h3>
-          <div className="flex gap-3">
-            <div className="flex items-center gap-1">
-              <BikeIcon className="text-primary" size={14} />
-              <span className="text-xs text-muted-foreground">
-                {Number(restaurant.deliveryFee) === 0
-                  ? "Entrega grátis"
-                  : formatCurrency(Number(restaurant.deliveryFee))}
-              </span>
-            </div>
+        <Button
+          size="icon"
+          className="absolute right-2 top-2 h-7 w-7 rounded-full bg-gray-700"
+        >
+          <HeartIcon size={16} className="fill-white" />
+        </Button>
+      </div>
 
-            <div className="flex items-center gap-1">
-              <TimerIcon className="text-primary" size={14} />
-              <span className="text-xs text-muted-foreground">
-                {restaurant.deliveryTimeMinutes} min.
-              </span>
-            </div>
+      <div>
+        <h3 className="text-sm font-semibold">{restaurant.name}</h3>
+        <div className="flex gap-3">
+          <div className="flex items-center gap-1">
+            <BikeIcon className="text-primary" size={14} />
+            <span className="text-xs text-muted-foreground">
+              {Number(restaurant.deliveryFee) === 0
+                ? "Entrega grátis"
+                : formatCurrency(Number(restaurant.deliveryFee))}
+            </span>
+          </div>
+
+          <div className="flex items-center gap-1">
+            <TimerIcon className="text-primary" size={14} />
+            <span className="text-xs text-muted-foreground">
+              {restaurant.deliveryTimeMinutes} min.
+            </span>
           </div>
         </div>
       </div>
-    </Link>
+    </div>
   );
 };
 
