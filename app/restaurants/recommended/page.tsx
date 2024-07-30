a31e54bdca80599a1fd7b5df09cb2ccadea9ad83
Leonardo Alves de Almeida <leonardoalves201510@gmail.com>
2024-07-29 21:29:40 -0300

    feat: add recommended restaurants page

--- app/restaurants/recommended/page.tsx
+++ app/restaurants/recommended/page.tsx
@@ -1,3 +1,4 @@
+import Header from "@/app/_components/header";
 import RestaurantItem from "@/app/_components/restaurant-item";
 import { db } from "@/app/_lib/prisma";
 
@@ -5,11 +6,23 @@ const RecommendedRestaurants = async () => {
   const restaurants = await db.restaurant.findMany({});
 
   return (
-    <div>
-      {restaurants.map((restaurant) => (
-        <RestaurantItem key={restaurant.id} restaurant={restaurant} />
-      ))}
-    </div>
+    <>
+      <Header />
+      <div className="px-5 py-6">
+        <h2 className="mb-6 text-lg font-semibold">
+          Restaurantes recomendados
+        </h2>
+        <div className="flex w-full flex-col">
+          {restaurants.map((restaurant) => (
+            <RestaurantItem
+              key={restaurant.id}
+              restaurant={restaurant}
+              className="min-w-full max-w-full"
+            />
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
