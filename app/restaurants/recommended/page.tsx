8048657797497d350529786f58917fb9149fd8e8
Leonardo Alves de Almeida <leonardoalves201510@gmail.com>
2024-08-02 23:05:13 -0300

    feat: add products page when user click in products category

--- app/restaurants/recommended/page.tsx
+++ app/restaurants/recommended/page.tsx
@@ -9,9 +9,7 @@ const RecommendedRestaurants = async () => {
     <>
       <Header />
       <div className="px-5 py-6">
-        <h2 className="mb-6 text-lg font-semibold">
-          Restaurantes recomendados
-        </h2>
+        <h2 className="text-lg font-semibold">Restaurantes recomendados</h2>
         <div className="flex w-full flex-col">
           {restaurants.map((restaurant) => (
             <RestaurantItem
