031b2729c354f5f864f7d43b1bf69bd1d2c614c8
Leonardo Alves de Almeida <leonardoalves201510@gmail.com>
2024-09-09 21:28:26 -0300

    chore: update import to deploy correctly

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import CategoryList from "./_components/category-list";
-import Header from "./_components/header";
 import ProductList from "./_components/product-list";
-import Search from "./_components/search";
 import { Button } from "./_components/ui/button";
 import { ChevronRightIcon } from "lucide-react";
 import { db } from "./_lib/prisma";
 import PromoBanner from "./_components/promo-banner";
 import RestaurantList from "./_components/restaurant-list";
 import Link from "next/link";
+import Header from "./_components/header";
+import Search from "./_components/search";
 
 const Home = async () => {
   const products = await db.product.findMany({
